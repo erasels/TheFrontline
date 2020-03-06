1124b5ecb2e954b6a86fa2a3424eecb01a949066
erasels <emanuelbedburdick@yahoo.de>
2020-03-06 18:31:26 +0100

    Added dynamic Map of all characters

--- theFrontline/src/main/java/theFrontline/util/CharacterHelper.java
+++ theFrontline/src/main/java/theFrontline/util/CharacterHelper.java
@@ -1,13 +1,64 @@
 package theFrontline.util;
 
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import theFrontline.TheFrontline;
 import theFrontline.characters.characterInfo.AbstractCharacterInfo;
+import theFrontline.characters.characterInfo.frontline.FrontlineInfo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class CharacterHelper {
+    /*public static HashMap<AbstractCharacterInfo.Rarity, HashMap<String, Class<? extends AbstractCharacterInfo>>> characterMap = new HashMap<AbstractCharacterInfo.Rarity, HashMap<String, Class<? extends AbstractCharacterInfo>>>() {{
+        put(AbstractCharacterInfo.Rarity.BASIC, new HashMap<>());
+        put(AbstractCharacterInfo.Rarity.COMMON, new HashMap<>());
+        put(AbstractCharacterInfo.Rarity.UNCOMMON, new HashMap<>());
+        put(AbstractCharacterInfo.Rarity.RARE, new HashMap<>());
+        put(AbstractCharacterInfo.Rarity.EPIC, new HashMap<>());
+    }};*/
+
+    public static HashMap<FrontlineInfo.Type, HashMap<String, FlInstanceInfo>> frontlineMap = new HashMap<FrontlineInfo.Type, HashMap<String, FlInstanceInfo>>() {{
+        put(FrontlineInfo.Type.AR, new HashMap<>());
+        put(FrontlineInfo.Type.HG, new HashMap<>());
+        put(FrontlineInfo.Type.MG, new HashMap<>());
+        put(FrontlineInfo.Type.RF, new HashMap<>());
+        put(FrontlineInfo.Type.SG, new HashMap<>());
+        put(FrontlineInfo.Type.SMG, new HashMap<>());
+    }};
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(TheFrontline.makeID("HelperStrings")).TEXT;
+
+    public static void addToMap(AbstractCharacterInfo ci) {
+        //characterMap.get(ci.rarity).put(ci.id, ci.getClass());
+    }
+
+    public static void addToMap(FrontlineInfo ci) {
+        frontlineMap.get(ci.type).put(ci.id, new FlInstanceInfo(ci.rarity, ci.getClass()));
+    }
+
+    public static FrontlineInfo retrieveCharacter(String id, FrontlineInfo.Type type) {
+        return frontlineMap.get(type).get(id).getChar();
+    }
+
+    public static FrontlineInfo getRandomCharacter(FrontlineInfo.Type type, AbstractCharacterInfo.Rarity rarity) {
+        ArrayList<FlInstanceInfo> tmp = frontlineMap.get(type).values().stream().filter(flInstanceInfo -> flInstanceInfo.rarity == rarity).collect(Collectors.toCollection(ArrayList::new));
+        tmp.removeIf(i -> i.rarity != rarity);
+        return tmp.get(AbstractDungeon.relicRng.random(tmp.size()-1)).getChar();
+    }
+
+    public static FrontlineInfo getRandomCharacter(FrontlineInfo.Type type) {
+        return getRandomCharacter(type, AbstractCharacterInfo.Rarity.values()[AbstractDungeon.relicRng.random(AbstractCharacterInfo.Rarity.values().length-1)]);
+    }
+
+    public static FrontlineInfo getRandomCharacter(AbstractCharacterInfo.Rarity rarity) {
+        return getRandomCharacter(FrontlineInfo.Type.values()[AbstractDungeon.relicRng.random(FrontlineInfo.Type.values().length-1)], rarity);
+    }
+
+    public static FrontlineInfo getRandomCharacter() {
+        return getRandomCharacter(FrontlineInfo.Type.values()[AbstractDungeon.relicRng.random(FrontlineInfo.Type.values().length-1)]);
+    }
 
     public static AbstractCharacterInfo getCharacterFromID(String id) {
         AbstractCharacterInfo ci = null;
@@ -46,5 +97,24 @@ public class CharacterHelper {
         String tmp = TEXT[7] + ci.getDescription();
         tmp = tmp.replaceAll("NL ", "\n");
         return tmp;
+    }
+
+    public static class FlInstanceInfo {
+        public FrontlineInfo.Rarity rarity;
+        public Class<? extends FrontlineInfo> fClass;
+
+        public FlInstanceInfo(FrontlineInfo.Rarity rarity, Class<? extends FrontlineInfo> fClass) {
+            this.rarity = rarity;
+            this.fClass = fClass;
+        }
+
+        public FrontlineInfo getChar() {
+            try {
+                return fClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
