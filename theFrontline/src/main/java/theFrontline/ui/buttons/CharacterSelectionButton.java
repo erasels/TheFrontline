cd80a8ed819a2be30f28810c4e592800a55db6a4
erasels <emanuelbedburdick@yahoo.de>
2020-03-08 13:03:47 +0100

    Create CharacterSelectionButton.java

---
+++ theFrontline/src/main/java/theFrontline/ui/buttons/CharacterSelectionButton.java
@@ -0,0 +1,33 @@
+package theFrontline.ui.buttons;
+
+import com.megacrit.cardcrawl.core.Settings;
+import theFrontline.characters.characterInfo.AbstractCharacterInfo;
+import theFrontline.patches.character.SelectionPatches;
+
+public class CharacterSelectionButton extends CharacterImageButton {
+    public CharacterSelectionButton(float x, float y, AbstractCharacterInfo ci) {
+        super(x, y, ci, false);
+    }
+
+    @Override
+    public void onClick() {
+        SelectionPatches.selectedChar = this;
+    }
+
+    @Override
+    public void onRightClick() {
+        SelectionPatches.backUpChar = this;
+    }
+
+    public AbstractCharacterInfo getChar() {
+        return ci;
+    }
+
+    public float getX() {
+        return x - ((ci.img.getWidth()/4f) * Settings.scale);
+    }
+
+    public float getY() {
+        return y;
+    }
+}
