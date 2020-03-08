8d91d871ee021f2c16126be25d4824449b92a776
erasels <emanuelbedburdick@yahoo.de>
2020-03-08 20:50:23 +0100

    Added CharaSelection HP render

--- theFrontline/src/main/java/theFrontline/ui/buttons/CharacterSelectionButton.java
+++ theFrontline/src/main/java/theFrontline/ui/buttons/CharacterSelectionButton.java
@@ -1,6 +1,10 @@
 package theFrontline.ui.buttons;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.screens.charSelect.CharacterOption;
 import theFrontline.characters.characterInfo.AbstractCharacterInfo;
 import theFrontline.patches.character.SelectionPatches;
 
@@ -21,6 +25,12 @@ public class CharacterSelectionButton extends CharacterImageButton {
 
     public AbstractCharacterInfo getChar() {
         return ci;
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        super.render(sb);
+        FontHelper.renderFontLeft(sb, FontHelper.tipHeaderFont, CharacterOption.TEXT[4] + ci.maxHP, x, y, Color.SALMON);
     }
 
     public float getX() {
