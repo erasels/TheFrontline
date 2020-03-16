bfd539fe4038f7f90e20780fa3797650b8e994c4
erasels <emanuelbedburdick@yahoo.de>
2020-03-16 22:25:36 +0100

    Update DoActionIfMonsterDeadAction.java

--- theFrontline/src/main/java/theFrontline/actions/utility/DoActionIfMonsterDeadAction.java
+++ theFrontline/src/main/java/theFrontline/actions/utility/DoActionIfMonsterDeadAction.java
@@ -2,18 +2,22 @@ package theFrontline.actions.utility;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.MinionPower;
 import theFrontline.util.UC;
 
 public class DoActionIfMonsterDeadAction extends AbstractGameAction {
     AbstractGameAction action;
+    private boolean skipMinions;
+
     public DoActionIfMonsterDeadAction(AbstractMonster m, AbstractGameAction action) {
         target = m;
         this.action = action;
+        skipMinions = true;
     }
 
     @Override
     public void update() {
-        if(target != null && target.isDeadOrEscaped()) {
+        if(target != null && target.isDeadOrEscaped() && (!skipMinions || !target.hasPower(MinionPower.POWER_ID))) {
             UC.att(action);
         }
 
