b0e3f911f4c36095785669c28fa81f90789f5a3d
erasels <emanuelbedburdick@yahoo.de>
2020-03-23 15:54:59 +0100

    Buffed Supression

--- theFrontline/src/main/java/theFrontline/cards/MG/Suppression.java
+++ theFrontline/src/main/java/theFrontline/cards/MG/Suppression.java
@@ -19,15 +19,14 @@ public class Suppression extends FrontlineCard {
     public final static String ID = makeID(cardInfo.cardName);
 
     private static final int DAMAGE = 8;
-    private static final int UPG_DAMAGE = 3;
+    private static final int UPG_DAMAGE = 4;
 
-    private static final int BLOCK = 8;
+    private static final int BLOCK = 9;
     private static final int UPG_BLOCK = 1;
 
 
     public Suppression() {
         super(cardInfo, false);
-        p(); //Stupid intellij stuff s, se
 
         setDamage(DAMAGE, UPG_DAMAGE);
         setBlock(BLOCK, UPG_BLOCK);
