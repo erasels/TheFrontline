dc7381533c996008fd866398d84ffbc99356aa1d
erasels <emanuelbedburdick@yahoo.de>
2020-07-06 18:46:27 +0200

    Provisionally removed this patch since it removed the music

--- theFrontline/src/main/java/theFrontline/patches/AntiAudioBufferCrashPatch.java
+++ theFrontline/src/main/java/theFrontline/patches/AntiAudioBufferCrashPatch.java
@@ -1,14 +1,11 @@
 package theFrontline.patches;
 
 import com.badlogic.gdx.backends.lwjgl.audio.OpenALMusic;
-import com.badlogic.gdx.utils.GdxRuntimeException;
-import com.evacipated.cardcrawl.modthespire.lib.*;
-import javassist.CtBehavior;
-import theFrontline.TheFrontline;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 
 @SpirePatch(clz = OpenALMusic.class, method = "play")
 public class AntiAudioBufferCrashPatch {
-    @SpireInsertPatch(locator = Locator.class, localvars = {"errorCode"})
+    /*@SpireInsertPatch(locator = Locator.class, localvars = {"errorCode"})
     public static SpireReturn patch(OpenALMusic __instance, int errorCode) {
         TheFrontline.logger.info("Intercepted audio buffer crash, Error Code: " + errorCode);
         return SpireReturn.Return(null);
@@ -20,5 +17,5 @@ public class AntiAudioBufferCrashPatch {
             Matcher finalMatcher = new Matcher.NewExprMatcher(GdxRuntimeException.class);
             return LineFinder.findInOrder(ctBehavior, finalMatcher);
         }
-    }
+    }*/
 }
